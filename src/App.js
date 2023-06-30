796355fa071dad350035bf43c5a72bf9cded0250
louiefalfonso <lfriasalfonso@gmail.com>
2023-06-30 14:31:42 +0100

    Kisdo Childcare Services Project
    Kisdo Childcare Services Project

--- src/App.js
+++ src/App.js
@@ -16,9 +16,6 @@ import SportsClass from './pages/SportsClass';
 import KnowledgeClass from './pages/KnowledgeClass';
 import ScienceClass from './pages/ScienceClass';
 
-import "core-js/es/set";
-import "core-js/es/weak-map";
-
 function App() {
   return (
     <div className="App">
